d11ffabfed68a005adcd1665f54960f6bc081be4
Renan Silva <renansupermanox@gmail.com>
2026-01-04 15:12:15 +0000

    global styles

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  #root {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
