87a16019a517800230eb126d6324573263853236
Renan Silva <renansupermanox@gmail.com>
2026-01-04 15:14:44 +0000

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import Board from "./components/Board";
+import TicTacToeGameProvider from "./context/tic-tac-toe-game-context";
+import { GlobalStyles } from "./styles/global-styles";
+
 export default function App() {
-  return <></>;
+  return (
+    <TicTacToeGameProvider>
+      <GlobalStyles />
+      <Board />
+    </TicTacToeGameProvider>
+  );
 }
